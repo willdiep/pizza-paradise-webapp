46a9563d3c6daca25ff11380718e1b7e8d85617f
wsdiep <widiep@gmail.com>
2016-07-28 23:18:09 -0700

    delete contact_form.js, contactlist.js(2), hello.js. remove scripts in app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var contactHandler = require('./handlers/contact_form')
 var pizzaParadise = require('./handlers/animation-pizza')
 var dropdownPizzaParadise = require('./handlers/dropdown_pizza_paradise')
-var helloHandler = require('./handlers/hello')
-var contactListHandler = require('./handlers/contactlist');
 
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static('static'));
-
 
 var ContextProxy = function(req,res){
 	this.req = req;
@@ -25,24 +21,6 @@ ContextProxy.prototype.done = function(err, data){
   this.res.status(200).send(data);
 }
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.post('/dynamo', function(req, res){
-  console.log("Inside Dynamo")
-  console.log(req.body);
-  context = new ContextProxy(req, res);
-  contactHandler(req.body, context);
-});
-
-
-app.get('/contacts', function(req, res){
-  console.log("Inside Contacts")
-  console.log(req.body);
-  context = new ContextProxy(req, res);
-  contactListHandler(req.body, context);
-});
 
 // animation-pizza - inputs that post to terminal and Dynamo
 app.post('/animationPizza', function(req, res){
@@ -60,17 +38,6 @@ app.post('/dropdownPizzaParadise', function(req, res){
   dropdownPizzaParadise(req.body, context);
 });
 
-app.post('/testing', function(req, res){
-  console.log("Inside Testing")
-  console.log(req.body);
-  context = new ContextProxy(req, res);
-  helloHandler(req.body, context);
-});
-
-app.post('/test', function(req, res){
-	console.log(req.body);
-	res.status(200).send("ok");
-});
 
 app.listen(3030, function () {
   console.log('Example app listening on port 3030!');
